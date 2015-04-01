69ef606d92a5a826f063bb4681414f8779797298
karrde00 <karrde00@gmail.com>
2015-04-01 11:49:20 -0500

    Adds commonJS wrapper

--- dist/utils.js
+++ dist/utils.js
@@ -1,28 +1,29 @@
+define( function(require, exports, module){
+    // return an array of the weeks (array of days) in a month
+  "use strict";
 
-// return an array of the weeks (array of days) in a month
-"use strict";
+  exports.weeks = weeks;
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
 
-exports.weeks = weeks;
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+  function weeks(m) {
 
-function weeks(m) {
+    var firstOfMonth = m.clone().startOf("month");
+    var lastOfMonth = m.clone().endOf("month");
 
-  var firstOfMonth = m.clone().startOf("month");
-  var lastOfMonth = m.clone().endOf("month");
+    var currentDay = firstOfMonth.clone();
+    var currentWeek = [];
+    var weeksInMonth = [];
 
-  var currentDay = firstOfMonth.clone();
-  var currentWeek = [];
-  var weeksInMonth = [];
-
-  while (currentDay < lastOfMonth) {
-    currentWeek.push(currentDay.clone());
-    currentDay.add(1, "d");
-    if (currentDay.weekday() === 0 || currentDay > lastOfMonth) {
-      weeksInMonth.push(currentWeek);
-      currentWeek = [];
+    while (currentDay < lastOfMonth) {
+      currentWeek.push(currentDay.clone());
+      currentDay.add(1, "d");
+      if (currentDay.weekday() === 0 || currentDay > lastOfMonth) {
+        weeksInMonth.push(currentWeek);
+        currentWeek = [];
+      }
     }
+    return weeksInMonth;
   }
-  return weeksInMonth;
-}
+});
